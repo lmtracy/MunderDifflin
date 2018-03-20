282b73ab52ac1310563693c2d219ede1de605e9a
Lauren Tracy <lauren.m.tracy@gmail.com>
2018-03-20 13:02:59 -0400

    removed non functioning js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,32 @@
-$('.mainText').each(function(){
+$('.ml11 .letters').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
 anime.timeline({loop: true})
   .add({
-    targets: '.mainText .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
     easing: "easeOutExpo",
-    duration: 950,
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0,$(".ml11 .letters").width()],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
     delay: function(el, i) {
-      return 70*i;
+      return 34 * (i+1)
     }
   }).add({
-    targets: '.mainText',
+    targets: '.ml11',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
