35250f0e4f1cd1a6c489c59a1558b0ef51770745
Lauren Tracy <lauren.m.tracy@gmail.com>
2018-04-02 20:10:53 -0400

    final

--- javascript.js
+++ javascript.js
@@ -1,34 +1,35 @@
-/* Navbar JS */
+/* NAVBAR JS */
 window.onscroll = () => {
   const nav = document.querySelector('#navbar');
   if(this.scrollY <= 700) nav.className = ''; else nav.className = 'scroll';
 };
 
 
-/* Slideshow */
-let slideIndex = 1;
-showSlides(slideIndex);
+/* SLIDESHOW JS */
+/*this code sets the order of items in the slideshow, runs if statements and for loops so users can scroll through  */
+let slideNumber = 1;
+showSlides(slideNumber);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideNumber += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideNumber = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("testiMony");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n < 1) {slideNumber = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  slides[slideNumber-1].style.display = "block";
+  dots[slideNumber-1].className += " active";
 }
